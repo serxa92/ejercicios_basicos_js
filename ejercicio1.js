6331b536b34406853a312721552b0419df0bd08d
Sergio Agulla <serxa92@hotmail.com>
2025-01-08 15:00:27 +0100

    subidos ejercicio1.js y ejercicio2.js

---
+++ ejercicio1.js
@@ -0,0 +1,49 @@
+// 1. Define tu superhéroe favorito:
+// Crea una variable llamada miSuperheroeFavorito y asigna el valor "Hulk".
+
+const miSuperheroeFavorito = "Hulk";
+
+// 2. Define un número:
+// Crea una variable llamada numeroFavorito y asigna el valor 50.
+
+const numeroFavorito = 50;
+
+// 3. Define las dimensiones:
+// Crea una variable llamada altura y asigna el valor 5.
+
+let altura = 5;
+
+// Crea una variable llamada longitud y asigna el valor 10.
+
+let longitud = 10;
+
+// 4. Calcula la suma:
+// Crea una variable llamada suma y asigna el valor de altura + longitud.
+
+let suma = altura + longitud;
+
+// 5. Imprime los resultados:
+// Muestra todas las variables por consola.
+
+console.log("Tu superhéroe favorito es : " + miSuperheroeFavorito);
+console.log("Tu numero favorito es :"+ numeroFavorito);
+console.log("La altura es : " + altura);
+console.log("La longitud es: " + longitud);
+console.log("La suma de altura y longitud es: " + suma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ejercicio2.js
@@ -0,0 +1,17 @@
+/* 1. Luke Skywalker cumple años:
+ Modifica el siguiente código JavaScript para actualizar la edad de Luke Skywalker a 25 años.*/
+
+
+ const jedi = {nombre: "Luke Skywalker", edad: 19};
+
+// Tu código para cambiar la edad aquí...
+
+jedi.edad = 20;
+
+/*Imprimo la edad de Luke Skywalker*/
+
+console.log("La edad de Luke Skywalker es: " + jedi.edad);
+
+/*Imprimo el objeto jedi para verificar que lo ha cambiado*/
+
+console.log(jedi);
