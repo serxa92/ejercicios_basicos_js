f836f06b49239b1299025e0fadaf549bb99943e9
Sergio Agulla <serxa92@hotmail.com>
2025-01-09 13:12:12 +0100

    ejercicio3

---
+++ ejercicio3.js
@@ -0,0 +1,55 @@
+/*3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+3.2 Divide 10 por 2 y muestra el resultado en un console.
+3.3 Muestra mediante un console el resto de dividir 15 por 9.
+3.4 Usa el correcto operador de asignación que resultará en o = 15,
+teniendo dos variables p = 10 y j = 5.
+3.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.*/
+
+
+//3.1
+
+let num1 = 10;
+
+let num2 = 5;
+
+let multiplicacion = num1 * num2;
+
+console.log(multiplicacion);
+
+
+
+
+
+
+//3.2
+
+num2 = 2;
+
+let division = num1 / num2;
+
+console.log(division);
+
+
+
+
+
+
+//3.3
+
+let num3 = 15;
+
+let num4 = 9;
+
+let resto = num3 % num4;
+
+console.log(resto);
+
+
+//3.4
+
+let p = 10;
+let j = 5;
+let o = p + j;
+console.log(o);
+
